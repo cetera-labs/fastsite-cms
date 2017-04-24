7a2d6b78dfd64e0baa863798049ceba341665dcc
Roman Romanov <nicodim@mail.ru>
2017-04-24 13:14:11 +0300

    BackOffice. Added "Help" menu.

--- cms/app/main/Header.js
+++ cms/app/main/Header.js
@@ -1,5 +1,5 @@
 Ext.define('Cetera.main.Header', {
-    extend: 'Ext.Container',
+    extend: 'Ext.Toolbar',
 
     id: 'app-header',
     height: 40,
@@ -14,17 +14,46 @@ Ext.define('Cetera.main.Header', {
             xtype: 'component',
             id: 'app-header-title',
             html: 'Cetera CMS'
-        },{
-            xtype: 'component',
-            id: 'app-header-site',
-			cls: 'header-item',
-            html: '<a href="/">'+ Config.Lang.toFrontOffice +'</a>',
-			flex: 1
-        },{
-            xtype: 'component',
+        },
+		{ xtype: 'tbseparator' },
+		{
+            xtype: 'button',
+			text: Config.Lang.toFrontOffice,
+			hrefTarget: '_top',
+            href: "/"
+        },	
+		'->',
+		{
+			iconCls: 'icon-help',
+			xtype: 'button',
+			text: _('Справка'),
+			menu: [{
+				text: _('Руководство пользователя'),
+				href: 'https://cetera.ru/forclients/cetera-cms-user-guide/',
+				hrefTarget: '_blank'
+			},{
+				text: _('Руководство разработчика'),
+				href: 'https://cetera.ru/forclients/cetera-cms-developer-guide/',
+				hrefTarget: '_blank'
+			},{
+				text: _('Вопросы и ответы'),
+				href: 'http://www.fastsite.ru/help/faq/',
+				hrefTarget: '_blank'
+			},{
+				text:'API',
+				href: 'https://cetera.ru/forclients/api/',
+				hrefTarget: '_blank'
+			}]
+		},
+		{ xtype: 'tbseparator' },
+		{
+            xtype: 'button',
             id: 'app-header-logout',
-            html: '<a href="logout.php?redirect=index.php">'+ Config.Lang.logout +'</a>'
-        }];
+			text: Config.Lang.logout,
+			hrefTarget: '_top',
+            href: "logout.php?redirect=index.php"
+        }
+		];
 
         this.callParent();
     }
