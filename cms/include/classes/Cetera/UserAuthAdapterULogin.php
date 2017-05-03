76f4aaf252383a5ae0ee262be696b54e76b88ee2
Roman Romanov <nicodim@mail.ru>
2017-05-03 16:06:57 +0300

    Bugfix in \Cetera\UserAuthAdapterULogin

--- cms/include/classes/Cetera/UserAuthAdapterULogin.php
+++ cms/include/classes/Cetera/UserAuthAdapterULogin.php
@@ -34,24 +34,16 @@ class UserAuthAdapterULogin extends ExternalUserAuthAdapter implements \Zend_Aut
         if ($this->user['last_name']) $name .= ' '.$this->user['last_name']; 
             
         $login = $this->user['uid'];
-            
-        User::getDbConnection()->insert(User::TABLE, array(
-            'date_reg'    => new \DateTime(), 
+		
+		$u = User::register(array(
             'login'       => $login, 
             'name'        => $name, 
             'email'       => $this->user['email'], 
-            'disabled'    => 0, 
-            'external'    => User::$social[$this->user['network']], 
-            'external_id' => $this->user['uid'],
-        ),array('datetime'));
-           
-        $fields = User::getDbConnection()->fetchAssoc( 'SELECT * FROM '.User::TABLE.' WHERE external=? and external_id=?', array( User::$social[$this->user['network']], $this->user['uid'] ) );
-        if ($fields)
-		{
-			$u = User::fetch($fields); 
-			$u->addExternal( $this->user['network'], $this->user['uid'] );
-			$u = User::getExternal($this->user['network'], $this->user['uid']);
-		}
+            'disabled'    => 0
+        ));
+            
+		$u->addExternal( $this->user['network'], $this->user['uid'] );
+		$u = User::getExternal($this->user['network'], $this->user['uid']);
 
         return $u;
     }
