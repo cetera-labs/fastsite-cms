6ccc72a4001c126d8b0b2949bf754db7a6dd527e
Roman Romanov <nicodim@mail.ru>
2017-05-05 08:39:32 +0300

    Widget 'List'. Added infinite loading feature.

--- cms/app/widget/List.js
+++ cms/app/widget/List.js
@@ -19,66 +19,88 @@ Ext.define('Cetera.widget.List', {
             maxValue: 999,
             minValue: 1,
             allowBlank: false
-        },{
-            name: 'order',
-            fieldLabel: Config.Lang.sort,
-            allowBlank: false
-        },new Ext.form.ComboBox({
-            fieldLabel: Config.Lang.order,
-            name:'sort',
-            store: new Ext.data.SimpleStore({
-                fields: ['name', 'value'],
-                data : [
-                    [_('Возрастанию'), 'ASC'],
-                    [_('Убыванию'), 'DESC']              
-                ]
-            }),
-            valueField:'value',
-            displayField:'name',
-            queryMode: 'local',
-            triggerAction: 'all',
-            editable: false,
-        }),
+        },
+		
+		{
+			xtype: 'fieldcontainer',
+			cls: 'x-field',
+			layout: 'hbox',
+			fieldLabel: Config.Lang.sort,
+			defaults: {
+				flex:1,
+				xtype: 'textfield',
+				hideLabel: true
+			},
+			items: [{
+				name: 'order',
+				allowBlank: false,
+				margin: '0 5 0 0'
+			},
+			new Ext.form.ComboBox({
+				name:'sort',
+				store: new Ext.data.SimpleStore({
+					fields: ['name', 'value'],
+					data : [
+						[_('Возрастанию'), 'ASC'],
+						[_('Убыванию'), 'DESC']              
+					]
+				}),
+				valueField:'value',
+				displayField:'name',
+				queryMode: 'local',
+				triggerAction: 'all',
+				editable: false,
+			})]
+			
+		},		
+		
 		{
             name: 'catalog_link',
             fieldLabel: _('Ссылка на раздел'),
         },
+		
 		{
 			xtype: 'fieldcontainer',
 			cls: 'x-field',
 			fieldLabel: _('Страницы'),
 			layout: 'hbox',
+			defaults: {
+				inputValue:     1,
+				uncheckedValue: 0,
+				margin: '0 5 0 0'				
+			},			
 			items: [{
 				xtype:          'checkbox',
 				boxLabel:       _('показать навигацию'),
-				name:           'paginator',
-				inputValue:     1,
-				uncheckedValue: 0,
-				flex: 1
+				name:           'paginator'
 			}, {
-				xtype: 'textfield',
-				name: 'page_param',
-				labelWidth: 150,
-				fieldLabel: _('query параметр'),
-				flex: 2
+				xtype:          'checkbox',
+				boxLabel:       _('AJAX навигация'),
+				name:           'ajax'
+			}, {
+				flex:1,
+				xtype:          'checkbox',
+				boxLabel:       _('бесконечная лента'),
+				name:           'infinite'
 			}]
 		},
 		{
 			xtype: 'fieldcontainer',
 			cls: 'x-field',
 			layout: 'hbox',
+			defaults: {
+				flex:1
+			},			
 			items: [{
-				xtype:          'checkbox',
-				boxLabel:       _('AJAX навигация'),
-				name:           'ajax',
-				inputValue:     1,
-				uncheckedValue: 0,
-				flex: 1
+				xtype: 'textfield',
+				name: 'page_param',
+				labelWidth: 150,
+				fieldLabel: _('query параметр'),
+				margin: '0 5 0 0'
 			}, {
 				xtype: 'textfield',
 				name: 'paginator_url',
-				fieldLabel: _('ссылка на страницу'),
-				flex: 2
+				fieldLabel: _('ссылка на страницу')
 			}]
 		},			
 		{
