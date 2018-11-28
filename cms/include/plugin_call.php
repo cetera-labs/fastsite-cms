63eea336aee64ce8f4483e74b62753e8df26d14b
Roman Romanov <nicodim@mail.ru>
2018-11-28 14:13:07 +0300

    https://pm.cetera.ru/browse/CCD-1303

--- cms/include/plugin_call.php
+++ cms/include/plugin_call.php
@@ -4,8 +4,10 @@ include(__DIR__.'/common.php');
 $p = parse_url($_SERVER['REQUEST_URI']);
 
 if (preg_match('|(/'.PLUGIN_DIR.'/[^/]*/)(.*)|',$p['path'],$m)) {
-
     define(PLUGIN_PATH, $m[1]);
-    require WWWROOT.PLUGIN_PATH.$m[2];
-    
+    require WWWROOT.PLUGIN_PATH.$m[2];    
+}
+elseif (preg_match('|(/'.THEME_DIR.'/[^/]*/)(.*)|',$p['path'],$m)) {
+    define(PLUGIN_PATH, $m[1]);
+    require WWWROOT.PLUGIN_PATH.$m[2];  	
 }
