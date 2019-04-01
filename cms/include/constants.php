b50abe18a4967d6b91e76e2967f6b225b291c83e
Roman Romanov <nicodim@mail.ru>
2019-04-01 15:04:27 +0300

    fastsite branch

--- cms/include/constants.php
+++ cms/include/constants.php
@@ -11,7 +11,7 @@
  **/
 
 /** Версия */
-define('VERSION', '3.59.8');
+define('VERSION', '3.59.8-fastsite');
 
 /** Название продукта */
 define('APP_NAME', 'Cetera CMS');
@@ -49,37 +49,15 @@ if (!defined('CMS_DIR'))
     //define('CMS_DIR', substr($_SERVER['SCRIPT_NAME'], 1, strpos($_SERVER['SCRIPT_NAME'],'/',1) - 1));
     define('CMS_DIR', 'cms' );
 	
+/** Абсолютный путь к корню CMS */
+define('CMSROOT', str_replace('include','',__DIR__) );
+
 /** Абсолютный путь DOCUMENT ROOT */
-//define('DOCROOT', $_SERVER['DOCUMENT_ROOT'].'/');
 if (!defined('DOCROOT')) {
-	if (isset($_SERVER['DOCUMENT_ROOT'])) {
-		$dr = $_SERVER['DOCUMENT_ROOT'].'/';
-	}
-	else {
-		$dr = substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'.CMS_DIR.'/', 1 )).'/';
-	}	
+	$dr = substr($_SERVER['SCRIPT_FILENAME'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'.CMS_DIR.'/', 1 )).'/';
 	define('DOCROOT', $dr );
 }
 define('WWWROOT', DOCROOT );
-
-define('LIBRARY_PATH', 'library');
-
-if (file_exists(DOCROOT.'../vendor/cetera-labs/cetera-cms')) {
-	define('COMPOSER_INSTALL', true);
-	define('VENDOR_PATH', DOCROOT.'../vendor');
-    define('CMSROOT', DOCROOT.'../vendor/cetera-labs/cetera-cms/cms/' );
-}
-else {
-	define('COMPOSER_INSTALL', false);
-	define('VENDOR_PATH', DOCROOT.'/'.LIBRARY_PATH.'/vendor');
-	
-	if (file_exists(DOCROOT.LIBRARY_PATH.'/library.php')) {
-		include_once(DOCROOT.LIBRARY_PATH.'/library.php');
-	} else {
-		define('LIBRARY_VERSION', 1);
-	}	
-    define('CMSROOT', str_replace('include','',__DIR__) );
-}
 
 define('PREFS_FILE',       DOCROOT.'.prefs');
 define('PREFS_FILE_LOCAL', DOCROOT.'.prefs.local');
@@ -118,6 +96,23 @@ define('IMAGECACHE_DIR', CACHE_DIR.'/images');
 define('TWIG_CACHE_DIR', CACHE_DIR.'/twig');
 define('USER_UPLOADS_DIR', DOCROOT.'uploads');
 define('USER_UPLOAD_PATH', '/uploads/');
+
+if (substr_count(__DIR__, 'vendor'.DIRECTORY_SEPARATOR.'cetera-labs'.DIRECTORY_SEPARATOR.'cetera-cms') > 0) {
+	define('COMPOSER_INSTALL', true);
+	define('LIBRARY_PATH', 'vendor/cetera-labs/library');
+	define('VENDOR_PATH', DOCROOT.'/vendor');
+}
+else {
+	define('COMPOSER_INSTALL', false);
+	define('LIBRARY_PATH', 'library');
+	define('VENDOR_PATH', DOCROOT.'/'.LIBRARY_PATH.'/vendor');
+	
+	if (file_exists(DOCROOT.LIBRARY_PATH.'/library.php')) {
+		include_once(DOCROOT.LIBRARY_PATH.'/library.php');
+	} else {
+		define('LIBRARY_VERSION', 1);
+	}	
+}
 
 // ----------- // файлы и каталоги -----------------
 
