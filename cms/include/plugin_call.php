1d6d9d748e2817d85ee51813649478302471d00d
Roman Romanov <nicodim@mail.ru>
2019-05-17 14:45:45 +0300

    Composer plugins

--- cms/include/plugin_call.php
+++ cms/include/plugin_call.php
@@ -3,9 +3,20 @@ namespace Cetera;
 include(__DIR__.'/common.php');
 $p = parse_url($_SERVER['REQUEST_URI']);
 
-if (preg_match('|(/'.PLUGIN_DIR.'/[^/]*/)(.*)|',$p['path'],$m)) {
+if (preg_match('|(/'.PLUGIN_DIR.'/([^/]*)/)(.*)|',$p['path'],$m)) {
     define('PLUGIN_PATH', $m[1]);
-    require WWWROOT.PLUGIN_PATH.$m[2];    
+    
+    if (file_exists(WWWROOT.PLUGIN_PATH.$m[3])) {
+        require WWWROOT.PLUGIN_PATH.$m[3];    
+    }
+    else {
+        $plugins = \Cetera\Plugin::enum();
+        if (isset($plugins[ $m[2] ])) {
+            if ( file_exists($plugins[ $m[2] ]->path . DIRECTORY_SEPARATOR . $m[3]) ) {
+                require($plugins[ $m[2] ]->path . DIRECTORY_SEPARATOR . $m[3]);
+            }
+        }
+    }
 }
 elseif (preg_match('|(/'.THEME_DIR.'/[^/]*/)(.*)|',$p['path'],$m)) {
     define('PLUGIN_PATH', $m[1]);
