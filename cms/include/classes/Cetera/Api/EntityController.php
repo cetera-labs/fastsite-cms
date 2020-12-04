0359f40ad5996443e9c1043d09159ae8395e3ee1
Roman Romanov <nicodim@mail.ru>
2020-12-04 11:28:28 +0300

    api

---
+++ cms/include/classes/Cetera/Api/EntityController.php
@@ -0,0 +1,57 @@
+<?php
+namespace Cetera\Api;
+
+use Zend\View\Model\JsonModel;
+use Cetera\Application;
+use Doctrine\Inflector\InflectorFactory;
+use Zend\Stdlib\RequestInterface as Request;
+use Zend\Stdlib\ResponseInterface as Response;  
+use Cetera\ORM\Paginator;
+
+class EntityController extends AbstractController
+{
+    private $inflector;
+    private $repository;
+    private $em;
+    private $entity;
+
+    public function dispatch(Request $request, Response $response = null)
+    {
+        $this->inflector = InflectorFactory::create()->build();
+        $this->em = Application::getInstance()->getEntityManager();
+        $this->entity = 'Cetera\Entity\\'.$this->inflector->classify( $this->getEvent()->getRouteMatch()->getParam('entity') );
+        $this->repository = $this->em->getRepository( $this->entity );
+        
+        return parent::dispatch($request, $response);
+    } 
+
+    public function defaultAction()
+    {
+        return $this->listAction();
+    }
+
+    public function listAction()
+    {
+        $query = $this->em->createQueryBuilder()
+                               ->select('e')
+                               ->from($this->entity, 'e')
+                               ->setFirstResult(0)
+                               ->setMaxResults(5);
+
+
+        $paginator = new Paginator($query);
+
+        $c = count($paginator);
+
+        $res = [
+            'success' => true,
+            'total' => (int)$c,
+            //'pages' => (int)$list->getPageCount(),            
+            'data' => $paginator->getIterator(),
+        ];
+        
+        return new JsonModel( $res );
+    }     
+    
+
+}
