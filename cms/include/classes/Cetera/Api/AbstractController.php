99b244230e4f0b9936f95ea19683f204b2ba0bef
Roman Romanov <nicodim@mail.ru>
2021-05-21 14:11:06 +0300

    dev

--- cms/include/classes/Cetera/Api/AbstractController.php
+++ cms/include/classes/Cetera/Api/AbstractController.php
@@ -41,7 +41,7 @@ class AbstractController extends AbstractRestfulController
         return new JsonModel([
             'success' => false,
             'error' => [
-                'message' => $exception->getMessage().' '.$exception->getFile().':'.$exception->getLine()
+                'message' => $exception->getMessage()
             ]
         ]);        
     }        
