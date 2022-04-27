c6e8ca39a1248ac8101401952d3f6245789c0866
Roman Romanov <nicodim@mail.ru>
2022-04-27 09:03:55 +0300

    https://pm.cetera.ru/browse/CLP-834

--- cms/include/classes/Cetera/Cache/Slot/MaterialField.php
+++ cms/include/classes/Cetera/Cache/Slot/MaterialField.php
@@ -19,4 +19,29 @@ class MaterialField extends Slot {
     public function __construct($table, $id, $field) {
         parent::__construct("material_{$table}_{$id}_{$field}", 3600 * 24);
     }
+	
+    public function load()
+    {
+        //$raw = $this->_getBackend()->load($this->_id);
+        //return unserialize($raw);
+		// TODO
+    }
+    
+    
+    /**
+     * Saves a data for this slot. 
+     * 
+     * @param mixed $data   Data to be saved.
+     * @return void
+     */
+    public function save($data)
+    {
+        //$tags = array();
+        //foreach ($this->_tags as $tag) {
+         //   $tags[] = $tag->getNativeId();
+        //}
+        //$raw = serialize($data);
+        //$this->_getBackend()->save($raw, $this->_id, $tags, $this->_lifetime);
+		// TODO
+    }	
 }
