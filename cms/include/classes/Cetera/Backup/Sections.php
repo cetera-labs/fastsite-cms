c1f96d1496786a712152e7523c6eed5db3af046f
Roman Romanov <nicodim@mail.ru>
2023-08-31 14:10:02 +0300

    Update Sections.php

--- cms/include/classes/Cetera/Backup/Sections.php
+++ cms/include/classes/Cetera/Backup/Sections.php
@@ -64,7 +64,12 @@ class Sections implements XmlInterface {
 				$parentSection = array_pop($stack);
 			}
 			if (in_array($xml->nodeType, array(\XMLReader::TEXT, \XMLReader::CDATA))) {
-                $fields[$field] .= $xml->value;	
+				if (!isset($fields[$field])) {
+					$fields[$field] = $xml->value;	
+				}
+				else {
+                			$fields[$field] .= $xml->value;	
+				}
 			}            
 		}		
 	}
