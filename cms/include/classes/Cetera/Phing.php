1e7233a47662d0767a8677095f3db74bd315049c
Baurzhan <baur.zhanabayev@gmail.com>
2026-05-14 13:08:03 +0500

    CRC-19155

--- cms/include/classes/Cetera/Phing.php
+++ cms/include/classes/Cetera/Phing.php
@@ -8,19 +8,35 @@ class Phing
     public static function plugins() {
             $vendorPath = dirname( dirname( dirname( dirname( dirname( dirname(__DIR__) ) ) ) ) );
             $res = [];
-            if (file_exists($vendorPath . DIRECTORY_SEPARATOR . 'cetera-labs' . DIRECTORY_SEPARATOR . 'cetera-cms-plugins.php')) {
-                $composer_plugins = include( $vendorPath . DIRECTORY_SEPARATOR . 'cetera-labs' . DIRECTORY_SEPARATOR . 'cetera-cms-plugins.php' );
-                foreach($composer_plugins as $k => $p) {
-                    self::$plugins[ $p['name'] ] = $vendorPath . DIRECTORY_SEPARATOR . $k;
-                    $res[] = $p['name'];
+
+            $file = $vendorPath . DIRECTORY_SEPARATOR . 'cetera-labs' . DIRECTORY_SEPARATOR . 'cetera-cms-plugins.php';
+
+            if (file_exists($file)) {
+
+                $composer_plugins = include($file);
+
+                if (is_array($composer_plugins)) {
+                    foreach ($composer_plugins as $k => $p) {
+
+                        if (!is_array($p)) {
+                            continue;
+                        }
+
+                        if (empty($p['name'])) {
+                            continue;
+                        }
+
+                        self::$plugins[$p['name']] = $vendorPath . DIRECTORY_SEPARATOR . $k;
+                        $res[] = $p['name'];
+                    }
                 }
             }
             
-            return implode(',',$res);
+            return $res ? implode(',', $res) : '';
     }
     
     public static function pluginPath($plugin) {
-        return self::$plugins[ $plugin ];
+        return self::$plugins[ $plugin ] ?? '';
     }
 
 }
