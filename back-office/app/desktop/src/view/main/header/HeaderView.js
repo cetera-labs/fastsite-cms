5008ba247f3dbbf69d07eb4b5c8b8d3a07034b4a
Roman Romanov <nicodim@mail.ru>
2026-09-22 19:19:59 +0500

    CCTM-1600 Убран битый пункт «Вопросы и ответы» из меню «Справка» админки

--- back-office/app/desktop/src/view/main/header/HeaderView.js
+++ back-office/app/desktop/src/view/main/header/HeaderView.js
@@ -98,10 +98,6 @@ Ext.define('Cetera.view.main.header.HeaderView', {
             text: _('Руководство разработчика'),
             href: 'https://cetera-labs.github.io/fastsite-cms/docs/developer-guide',
             hrefTarget: '_blank'
-        },{
-            text: _('Вопросы и ответы'),
-            href: 'http://www.fastsite.ru/help/faq/',
-            hrefTarget: '_blank'
         }]
     },
     { xtype: 'tbseparator' },
