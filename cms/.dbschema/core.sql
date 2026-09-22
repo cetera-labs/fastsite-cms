b8c713fc615cd3a33d81ad6405fd12a56a0562f5
Roman Romanov <nicodim@mail.ru>
2026-09-22 20:27:56 +0500

    CCTM-1594 После установки CMS описания типов материалов совпадают со схемой ядра

--- cms/.dbschema/core.sql
+++ cms/.dbschema/core.sql
@@ -19,11 +19,11 @@ INSERT INTO `types_fields`
 (1, 1, 'name', 1,              'Title[ru=Заголовок]',        1024, 1, 0, 1, 2,  0, NULL, 0, NULL, NULL),
 (8, 1, 'alias', 1,             'Alias',            255,  1, 1, 1, 3,  0, NULL, 0, NULL, NULL),
 (7, 1, 'autor', 6,             'Author[ru=Автор]',            -1,   1, 1, 1, 4,  1003, NULL, 0, NULL, NULL),
-(4, 1, 'dat_update', 5,        'Edit date[ru=Дата редактирования]',1,    1, 1, 0, 5,  0, NULL, 0, NULL, NULL),
-(3, 1, 'dat', 5,               'Creation date[ru=Дата создания]',    1,    1, 1, 0, 6,  0, NULL, 0, NULL, NULL),
-(2, 1, 'idcat', 6,             'Section[ru=Раздел]',           0,    1, 1, 0, 7,  1008, NULL, 0, NULL, NULL),
+(4, 1, 'dat_update', 5,        'Edit date[ru=Дата редактирования]',1,    1, 1, 0, 6,  0, NULL, 0, NULL, NULL),
+(3, 1, 'dat', 5,               'Creation date[ru=Дата создания]',    1,    1, 1, 0, 5,  0, NULL, 0, NULL, NULL),
+(2, 1, 'idcat', 6,             'Section[ru=Раздел]',           0,    1, 1, 0, 8,  1008, NULL, 0, NULL, NULL),
 (5, 1, 'tag', 3,               'Sort[ru=Сортировка]',       1,    1, 0, 1, 1,  0, 100, 0, NULL, NULL),
-(6, 1, 'type', 3,              '',                 1,    1, 1, 0, 8,  0, NULL, 0, NULL, NULL),
+(6, 1, 'type', 3,              '',                 1,    1, 1, 0, 7,  0, NULL, 0, NULL, NULL),
 (9, 1, 'meta_title', 1,        'Meta title',       1000, 0, 0, 1, 13, 0, '', 1, '', 'SEO'),
 (10, 1, 'meta_keywords', 1,    'Meta keywords',    1000, 0, 0, 1, 14, 0, '', 16, '', 'SEO'),
 (11, 1, 'meta_description', 1, 'Meta description', 1000, 0, 0, 1, 15, 0, '', 16, '', 'SEO'),
@@ -35,10 +35,11 @@ INSERT INTO `types_fields`
 (22, 4, 'meta_keywords', 1,    'Meta keywords', 1000, 0, 0, 1, 12, 0, '', 1, '', 'SEO'),
 (23, 4, 'meta_description', 1, 'Meta description', 1000, 0, 0, 1, 13, 0, '', 16, '', 'SEO'),
 (24, 4, 'pic', 4,              'Picture[ru=Картинка]', 1, 0, 0, 1, 14, 0, '', 4, '', NULL),
+(25, 4, 'visual_constructor', 14, 'Constructor[ru=Конструктор]', 1, 1, 0, 0, 15, 0, '', 0, '', NULL),
 
 (38, 2, 'tag', 3,        'Sort[ru=Сортировка]',       1, 1, 0, 1, 1,  0, 100,  0, NULL, NULL),
-(30, 2, 'login', 1,      'Login',                   255, 1, 1, 1, 30, 0, NULL, 0, NULL, NULL),
-(31, 2, 'name', 1,       'Name[ru=Имя]', 255, 1, 0, 1, 31, 0, NULL, 0, NULL, NULL),
+(30, 2, 'login', 1,      'Login',                   255, 1, 1, 1, 2, 0, NULL, 0, NULL, NULL),
+(31, 2, 'name', 1,       'Name[ru=Имя]', 255, 1, 0, 1, 3, 0, NULL, 0, NULL, NULL),
 (32, 2, 'password', 1,   'Password', 32, 1, 1, 1, 32, 0, NULL, 27, NULL, NULL),
 (33, 2, 'email', 1,      'E-mail', 255, 1, 0, 1, 33, 0, NULL, 28, NULL, NULL),
 (34, 2, 'describ', 1,    'Description[ru=Описание]', 1000, 1, 0, 1, 34, 0, NULL, 15, NULL, NULL),
@@ -112,11 +113,12 @@ CREATE TABLE IF NOT EXISTS `dir_data` (
   `pic` varchar(1024) DEFAULT NULL,
   `meta_title` varchar(1000) DEFAULT NULL,
   `meta_keywords` varchar(1000) DEFAULT NULL,
-  `meta_description` varchar(1000) DEFAULT NULL,  
+  `meta_description` varchar(1000) DEFAULT NULL,
+  `visual_constructor` longtext DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO dir_data VALUES ( 1, 1, 'default', 'default', 7, '', 1, NOW(), 0, 1, 'themes/default', 1, '', '', '', '');
+INSERT INTO dir_data VALUES ( 1, 1, 'default', 'default', 7, '', 1, NOW(), 0, 1, 'themes/default', 1, '', '', '', '', NULL);
 
 INSERT INTO `mail_templates` (`event`, `active`, `content_type`, `mail_subject`, `mail_body`, `mail_from_name`, `mail_from_email`, `mail_to`) VALUES
 ('USER_RECOVER', 1, 'text/plain', "{{ _('Ваш пароль на сайт') }} {{server.name}}", "{{ _('Ваш новый пароль: ') }} {{password}}", '', 'no-reply@cetera.ru', '{{user.email}}');
